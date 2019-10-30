4197f17a303d14549042c2a1f7573dbd380992c7
waqas740 <waqasiqbal740@gmail.com>
2019-10-31 04:29:17 +0500

    slack message

--- index.js
+++ index.js
@@ -2,11 +2,34 @@ const request = require('request-promise');
 const _ = require('lodash');
 
 class SlackNotify {
-    constructor(client_id) {
-      this.client_id = client_id
-    }
-    authentication(){
-        location = "https://slack.com/oauth/v2/authorize?scope=incoming-webhook,commands&client_id="+this.client_id
+    constructor(access_token) {
+     // this.client_id = client_id;
+      this.access_token = access_token
+      //this.authUrl = "https://slack.com/oauth/v2/authorize?client_id="+this.client_id;
     }
     
+     async sendMessage(channel_id,message) {
+      var options = {
+        method: 'POST',
+        uri: 'https://slack.com/api/chat.postMessage',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': "Bearer "+this.access_token
+       },
+        body:{
+          "channel": channel_id,
+          "text": message
+      } ,
+        json: true // Automatically stringifies the body to JSON
+       
+    };
+    return await request(options);
+ 
+     
+    }
+   
+    
+}
+module.exports = (access_token) => {
+  return new SlackNotify(access_token)
 }
